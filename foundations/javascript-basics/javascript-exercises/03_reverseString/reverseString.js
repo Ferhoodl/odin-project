04ee547d200e6fce41674dd75c21f333c5e55340
ferhoodl <isaiah.w.steele@gmail.com>
2024-09-26 08:25:43 -0500

    Complete exercise 03

--- foundations/javascript-basics/javascript-exercises/03_reverseString/reverseString.js
+++ foundations/javascript-basics/javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+	let revString = '';
+	for(let i = word.length; i > 0; i--){
+		revString += word[i - 1];
+	}
+	return revString;
 };
 
 // Do not edit below this line
