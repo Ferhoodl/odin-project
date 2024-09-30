04e71e7dfee375105e8082c4ab27b2ca76dd4cfd
ferhoodl <isaiah.w.steele@gmail.com>
2024-09-30 01:09:29 -0500

    Complete 'removeFromArray' exercise

--- foundations/javascript-basics/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ foundations/javascript-basics/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayThings, ...opts) {
+	let newArray = [];
+	for(const obj of arrayThings){
+		if (opts.includes(obj)){
+			continue;
+		}else{
+			newArray.push(obj);
+		}
+	}
+	return newArray;
 };
 
 // Do not edit below this line
