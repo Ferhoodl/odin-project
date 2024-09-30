f6ff6117cb3d02892cbfb5b9e1abf7a089da779d
ferhoodl <isaiah.w.steele@gmail.com>
2024-09-30 01:39:31 -0500

    Complete 'sumall' javascript exercise

--- foundations/javascript-basics/javascript-exercises/05_sumAll/sumAll.js
+++ foundations/javascript-basics/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+	let sum = 0;
+	if (start > finish){
+		let temp = start;
+		start = finish;
+		finish = temp;
+	}
+	if (!(start >= 0) || !(finish >= 0)){
+		return "ERROR";
+	}else if (!(typeof start == "number") || !(typeof finish == "number")){
+		return "ERROR";
+	}else if((start % 1 != 0) || (finish % 1 != 0)){
+		return "ERROR";
+	}
+	else{
+		for (let i = start; i <= finish; i++){
+			sum += i;
+		}
+		return sum;
+	}
 };
 
 // Do not edit below this line
